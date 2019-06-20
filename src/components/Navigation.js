a5ecfeb949923a4ba29c40a088d64d024466872a
mouri <tt.mouri@gmail.com>
2019-06-20 11:50:03 +0900

    InitialCommit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavigationBar = styled.div`
+  margin-bottom: 15px;
+  background-color: lightgray;
+`;
 
 export default () => (
-  <div>
+  <NavigationBar>
     <Link className="btn btn-primary" to="/">
       一覧
     </Link>
     <Link className="btn btn-secondary" to="/new-item">
       新規メモ登録
     </Link>
-  </div>
+  </NavigationBar>
 );
